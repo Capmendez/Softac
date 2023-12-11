385d6ef8976e1de948abff1e432ef084f7973082
Capmendez <capmendez078@gmail.com>
2023-12-11 20:59:55 +0100

    softac

---
+++ src/layout/headers/nav-menu.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+import menu_data from "./menu-data";
+
+const NavMenu = () => {
+  return (
+    <>
+      <ul>
+        {menu_data.map((menu_item, i) => (
+          <li key={i}>
+            <Link href={menu_item.link}>{menu_item.title}</Link>
+            {menu_item.has_dropdown && (
+              <ul className="submenu">
+                
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default NavMenu;
